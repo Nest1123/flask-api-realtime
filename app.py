65dca0cb6561d7434d6c19578718e326876e8a75
Nest1123 <130137700+Nest1123@users.noreply.github.com>
2025-04-10 13:50:58 +0700

    upload first files
    upload first files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # ให้เว็บภายนอกเรียกใช้ API ได้
+
+latest_data = {}  # เก็บข้อมูลล่าสุด
+
+@app.route('/api/data', methods=['POST'])
+def receive_data():
+    global latest_data
+    latest_data = request.json
+    return jsonify({'message': 'Data received'}), 200
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify(latest_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
